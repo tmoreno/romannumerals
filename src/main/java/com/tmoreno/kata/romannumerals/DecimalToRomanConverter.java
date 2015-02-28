cfe9b7598565da7089ef9c192c8daca982dae49a
tmoreno <tmorenobernal@yahoo.es>
2015-02-28 20:17:14 +0100

    Return to String class

--- src/main/java/com/tmoreno/kata/romannumerals/DecimalToRomanConverter.java
+++ src/main/java/com/tmoreno/kata/romannumerals/DecimalToRomanConverter.java
@@ -3,22 +3,22 @@ package com.tmoreno.kata.romannumerals;
 public class DecimalToRomanConverter {
 
 	public String convert(int decimal) {
-		StringBuilder result = new StringBuilder();
+		String result = "";
 
 		if (decimal < 4) {
 			for (int i = 0; i < decimal; i++) {
-				result.append("I");
+				result += "I";
 			}
 		} else if (decimal == 4) {
-			result.append("IV");
+			result += "IV";
 		} else {
-			result.append("V");
+			result += "V";
 
 			for (int i = 5; i < decimal; i++) {
-				result.append("I");
+				result += "I";
 			}
 		}
 
-		return result.toString();
+		return result;
 	}
 }
