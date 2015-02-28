163054359e14e0dfdda031bfafd3d2470d4c9482
tmoreno <tmorenobernal@yahoo.es>
2015-02-28 17:04:41 +0100

    Refactor in my tests
    Green.

--- src/test/java/com/tmoreno/kata/romannumerals/TestCase.java
+++ src/test/java/com/tmoreno/kata/romannumerals/TestCase.java
@@ -1,20 +1,26 @@
 package com.tmoreno.kata.romannumerals;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestCase {
 
+    private DecimalToRomanConverter converter;
+
+    @Before
+    public void init() {
+        converter = new DecimalToRomanConverter();
+    }
+
     @Test
     public void given1getI() {
-        DecimalToRomanConverter converter = new DecimalToRomanConverter();
         String result = converter.convert(1);
         Assert.assertEquals("I", result);
     }
 
     @Test
     public void given2getII() {
-        DecimalToRomanConverter converter = new DecimalToRomanConverter();
         String result = converter.convert(2);
         Assert.assertEquals("II", result);
     }
