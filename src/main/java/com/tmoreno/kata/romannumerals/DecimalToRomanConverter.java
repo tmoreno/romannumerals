9de5549a9c11dc888d322e4f526befe25bd80dca
tmoreno <tmorenobernal@yahoo.es>
2015-02-28 22:34:54 +0100

    Replace for a equivalent method

--- src/main/java/com/tmoreno/kata/romannumerals/DecimalToRomanConverter.java
+++ src/main/java/com/tmoreno/kata/romannumerals/DecimalToRomanConverter.java
@@ -5,7 +5,7 @@ public class DecimalToRomanConverter {
 	public String convert(int decimal) {
 		if (decimal >= 10) {
 			if (decimal < 14) {
-				return "X" + addI(decimal % 10);
+				return "X" + convertUnits(decimal % 10);
 			} else {
 				return "X" + convertUnits(4);
 			}
