3ece9b4c93e8f889885e3d7881ba8f3b37510425
tmoreno <tmorenobernal@yahoo.es>
2015-02-28 20:15:59 +0100

    Numbers 1 and 5 is covered by the default part of the switch

--- src/main/java/com/tmoreno/kata/romannumerals/DecimalToRomanConverter.java
+++ src/main/java/com/tmoreno/kata/romannumerals/DecimalToRomanConverter.java
@@ -3,29 +3,22 @@ package com.tmoreno.kata.romannumerals;
 public class DecimalToRomanConverter {
 
 	public String convert(int decimal) {
-		switch (decimal) {
-		case 1:
-			return "I";
-		case 5:
-			return "V";
-		default:
-			StringBuilder result = new StringBuilder();
+		StringBuilder result = new StringBuilder();
 
-			if (decimal < 4) {
-				for (int i = 0; i < decimal; i++) {
-					result.append("I");
-				}
-			} else if (decimal == 4) {
-				result.append("IV");
-			} else {
-				result.append("V");
-
-				for (int i = 5; i < decimal; i++) {
-					result.append("I");
-				}
+		if (decimal < 4) {
+			for (int i = 0; i < decimal; i++) {
+				result.append("I");
 			}
+		} else if (decimal == 4) {
+			result.append("IV");
+		} else {
+			result.append("V");
 
-			return result.toString();
+			for (int i = 5; i < decimal; i++) {
+				result.append("I");
+			}
 		}
+
+		return result.toString();
 	}
 }
