462d39ae265d83e4889ee807f9fa1c00fae09e6e
tmoreno <tmorenobernal@yahoo.es>
2015-02-28 17:10:59 +0100

    My tests are green, so I can do a refactor.
    Refactor > Green

--- src/main/java/com/tmoreno/kata/romannumerals/DecimalToRomanConverter.java
+++ src/main/java/com/tmoreno/kata/romannumerals/DecimalToRomanConverter.java
@@ -2,15 +2,13 @@ package com.tmoreno.kata.romannumerals;
 
 public class DecimalToRomanConverter {
 
-    public String convert(int i) {
-        if (i == 1) {
-            return "I";
+    public String convert(int decimal) {
+        String result = "";
+
+        for (int i = 0; i < decimal; i++) {
+            result += "I";
         }
-        else if (i == 2) {
-            return "II";
-        }
-        else {
-            return "III";
-        }
+
+        return result;
     }
 }
