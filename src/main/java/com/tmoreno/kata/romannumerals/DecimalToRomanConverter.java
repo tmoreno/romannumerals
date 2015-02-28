bcca58217c0d61c244d285e6d3da65f5f1c1b214
tmoreno <tmorenobernal@yahoo.es>
2015-02-28 19:57:40 +0100

    I have written the minimum code for pass the tests.
    Green

--- src/main/java/com/tmoreno/kata/romannumerals/DecimalToRomanConverter.java
+++ src/main/java/com/tmoreno/kata/romannumerals/DecimalToRomanConverter.java
@@ -3,17 +3,24 @@ package com.tmoreno.kata.romannumerals;
 public class DecimalToRomanConverter {
 
     public String convert(int decimal) {
-        StringBuilder result = new StringBuilder();
+        switch (decimal) {
+		case 1:
+			return "I";
+		case 5:
+			return "V";
+		default:
+			StringBuilder result = new StringBuilder();
+			
+			for (int i = 0; i < decimal; i++) {
+	            result.append("I");
+	        }
+	        
+	        if (decimal > 3) {
+	        	result.setLength(1);
+	        	result.append("V");
+	        }
 
-        for (int i = 0; i < decimal; i++) {
-            result.append("I");
-        }
-        
-        if (decimal > 3) {
-        	result.setLength(1);
-        	result.append("V");
-        }
-
-        return result.toString();
+	        return result.toString();
+		}
     }
 }
