513323b4694a56e7a0fa360e4c389199ac4facd8
tmoreno <tmorenobernal@yahoo.es>
2015-02-28 20:12:06 +0100

    Now, I distinguish between major and minor numbers that 4

--- src/main/java/com/tmoreno/kata/romannumerals/DecimalToRomanConverter.java
+++ src/main/java/com/tmoreno/kata/romannumerals/DecimalToRomanConverter.java
@@ -2,25 +2,30 @@ package com.tmoreno.kata.romannumerals;
 
 public class DecimalToRomanConverter {
 
-    public String convert(int decimal) {
-        switch (decimal) {
+	public String convert(int decimal) {
+		switch (decimal) {
 		case 1:
 			return "I";
 		case 5:
 			return "V";
 		default:
 			StringBuilder result = new StringBuilder();
-			
-			for (int i = 0; i < decimal; i++) {
-	            result.append("I");
-	        }
-	        
-	        if (decimal > 3) {
-	        	result.setLength(1);
-	        	result.append("V");
-	        }
 
-	        return result.toString();
+			if (decimal < 4) {
+				for (int i = 0; i < decimal; i++) {
+					result.append("I");
+				}
+			} else if (decimal == 4) {
+				result.append("IV");
+			} else {
+				result.append("V");
+
+				for (int i = 5; i < decimal; i++) {
+					result.append("I");
+				}
+			}
+
+			return result.toString();
 		}
-    }
+	}
 }
