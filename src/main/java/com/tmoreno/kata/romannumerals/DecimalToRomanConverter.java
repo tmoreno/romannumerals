fa3ed47ccd424ad18b91bfdcd66f5eb0cdf6bcf7
tmoreno <tmorenobernal@yahoo.es>
2015-03-01 18:56:43 +0100

    Refactor

--- src/main/java/com/tmoreno/kata/romannumerals/DecimalToRomanConverter.java
+++ src/main/java/com/tmoreno/kata/romannumerals/DecimalToRomanConverter.java
@@ -7,7 +7,7 @@ public class DecimalToRomanConverter {
 
 		if (decimal >= 10) {
 			if (decimal >= 10 && decimal < 40) {
-				result += addLetterNTimes("X", decimal / 10);
+				result = addLetterNTimes("X", decimal / 10);
 			} else if (decimal >= 40 && decimal < 50) {
 				result = "XL";
 			} else if (decimal >= 50 && decimal < 90) {
@@ -20,20 +20,28 @@ public class DecimalToRomanConverter {
 		return result + convertUnits(decimal % 10);
 	}
 
-	private String convertUnits(int number) {
-		String result = "";
+	private String convertUnits(int unit) {
+		switch (unit) {
+		case 1:
+		case 2:
+		case 3:
+			return addLetterNTimes("I", unit);
 
-		if (number < 4) {
-			result += addLetterNTimes("I", number);
-		} else if (number == 4) {
-			result += "IV";
-		} else if (number == 9) {
-			result += "IX";
-		} else {
-			result += "V" + addLetterNTimes("I", number % 5);
+		case 4:
+			return "IV";
+
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+			return "V" + addLetterNTimes("I", unit % 5);
+
+		case 9:
+			return "IX";
+
+		default:
+			return "";
 		}
-
-		return result;
 	}
 
 	private String addLetterNTimes(String letter, int numbresOftimes) {
