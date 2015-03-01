772d18414ce0758fca56b278973a70cd137d2919
tmoreno <tmorenobernal@yahoo.es>
2015-03-01 20:01:50 +0100

    These tests are correct
    Green

--- src/test/java/com/tmoreno/kata/romannumerals/NumbersGreaterThan1000.java
+++ src/test/java/com/tmoreno/kata/romannumerals/NumbersGreaterThan1000.java
@@ -18,4 +18,22 @@ public class NumbersGreaterThan1000 {
 		String result = converter.convert(1000);
 		Assert.assertEquals("M", result);
 	}
+
+	@Test
+	public void given1001getMI() {
+		String result = converter.convert(1001);
+		Assert.assertEquals("MI", result);
+	}
+
+	@Test
+	public void given2000getMM() {
+		String result = converter.convert(2000);
+		Assert.assertEquals("MM", result);
+	}
+
+	@Test
+	public void given2015getMMXV() {
+		String result = converter.convert(2015);
+		Assert.assertEquals("MMXV", result);
+	}
 }
